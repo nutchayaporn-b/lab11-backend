cf05d208e44718de2e0f4433c1bf61d960538956
Nutchayaporn Buarom <nutchayaporn_b@cmu.ac.th>
2022-09-30 00:41:25 +0700

    1.9

--- src/main/java/se331/rest/repository/EventRepository.java
+++ src/main/java/se331/rest/repository/EventRepository.java
@@ -15,4 +15,5 @@ public interface EventRepository extends JpaRepository<Event,Long> {
     Page<Event> findByTitleContainingOrDescriptionContaining(String title, String description, Pageable pageRequest);
     Page<Event> findByTitleContainingAndDescriptionContaining(String title, String description, Pageable pageRequest);
     Page<Event> findByTitleContainingOrDescriptionContainingOrOrganizer_NameContaining(String title, String description, String organizerName, Pageable pageRequest);
+    Page<Event> findByTitleIgnoreCaseContainingOrDescriptionIgnoreCaseContainingOrOrganizer_NameIgnoreCaseContaining(String title, String description, String organizerName, Pageable pageRequest);
 }
